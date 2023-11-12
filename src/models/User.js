061e6161aa8a1d3fb9e120bc7a8620fd740bc29b
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2023-11-12 00:28:34 -0300

    Change how to create user

--- src/models/User.js
+++ src/models/User.js
@@ -1,30 +1,33 @@
 import mongoose from "../db/conn.js";
 import { Schema } from "mongoose";
 
-export const User = mongoose.model(
-  "User",
-  new Schema(
-    {
-      name: {
-        type: String,
-        required: true,
-      },
-      email: {
-        type: String,
-        required: true,
-      },
-      password: {
-        type: String,
-        required: true,
-      },
-      image: {
-        type: String,
-      },
-      phone: {
-        type: String,
-        required: true,
-      },
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    { timestamps: true },
-  )
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+    },
+    image: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
 );
+let User = mongoose.model("User", userSchema);
+
+export default User;
