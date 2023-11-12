6135043f50089e6f382536dca9f30589240d210f
Caíque Brener <65732426+C-Brener@users.noreply.github.com>
2023-11-12 00:30:06 -0300

    Listen new Route

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import express from "express";
 import cors from "cors";
+import UserRoutes from "./routes/UserRoutes.js";
 
-
-const app = express()
+const app = express();
 
 //Express
-app.use(express.json())
+app.use(express.json());
 
 //Cors
-app.use(cors({credentials:true, origin:"http://localhost:3000"}))
+app.use(cors({ credentials: true, origin: "http://localhost:5000"}));
 
 //Public Images
-app.use(express.static('public'))
-
+app.use(express.static("public"));
 
 //Routes
-app.listen(5000)
+app.use("/users", UserRoutes);
+
+app.listen(5000, () => {
+  console.log("Servidor rodando na porta 5000");
+});
